975fe21254ed0f048af9e0e8437c6cf65a13cc6d
jinhee220 <jin.lee220@gmail.com>
2024-05-06 14:59:57 -0400

    attempting deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+});
+
+// This is used for deploying to a project
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/my-project/'
+//     : '/'
+// }
