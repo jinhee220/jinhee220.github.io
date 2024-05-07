33e7ad61a3f47e6100aef8a844c7a664ff23d8e8
jinhee220 <jin.lee220@gmail.com>
2024-05-07 18:01:54 -0400

    config restest

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/jinhee220/jinhee220.github.io/",
   plugins: [vue()],
 });
 
@@ -12,3 +13,18 @@ export default defineConfig({
 //     ? '/my-project/'
 //     : '/'
 // }
+
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// });
+
+// // This is used for deploying to a project
+// // module.exports = {
+// //   publicPath: process.env.NODE_ENV === 'production'
+// //     ? '/my-project/'
+// //     : '/'
+// // }
