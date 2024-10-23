4957b058d206620395d10d681aafdcb510f1643c
jinhee220 <jin.lee220@gmail.com>
2024-10-22 22:44:42 -0400

    run build fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import VueScrollTo from "vue-scrollto";
-
-vue.use(VueScrollTo);
 
 // https://vitejs.dev/config/
 export default defineConfig({
